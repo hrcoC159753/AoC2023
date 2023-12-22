9f35093249c5c9c82d2a50a89a1e339eb1f28fb6
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2023-12-22 09:35:53 -0500

    Another try to solve part 2

--- Day21/part2.py
+++ Day21/part2.py
@@ -1,9 +1,10 @@
 import sys
 import itertools
+import more_itertools
 import functools
 
 # file = sys.stdin
-file = open('input1.txt')
+file = open('Day21/input1.txt')
 
 def findS(lines):
     for i in range(len(lines)):
@@ -20,17 +21,7 @@ def accessLines(lines, y, x):
 
 def isValidPosition(position, lines):
     y, x = position
-
-    # if y < 0 or y >= len(lines):
-    #     return False
-
-    # if x < 0 or x >= len(lines[0]):
-    #     return False
-
-    if accessLines(lines, y, x) == '#':
-        return False
-
-    return True
+    return not (accessLines(lines, y, x) == '#')
 
 def calculateGridPosition(position, lines):
     return (position[0] // len(lines), position[1] // len(lines[0]))
@@ -41,110 +32,97 @@ def getNextPositions(currentPosition, lines):
 
     return [possiblePosition for possiblePosition in possiblePositions if isValidPosition(possiblePosition, lines)]
 
-def solveFromPosition(currentPosition, lines, numberOfRepeats):
-    listOfPositions = set()
-    listOfPositions.add(currentPosition)
+def getNextPositionsMult(lines, currentPositions):
+    return {*itertools.chain.from_iterable(getNextPositions(position, lines) for position in currentPositions)}
 
-    while numberOfRepeats != 0:
-        newPositions = set()
-        for position in itertools.chain.from_iterable(getNextPositions(position, lines) for position in listOfPositions):
-            newPositions.add(position)
-        listOfPositions = newPositions
-        numberOfRepeats -= 1
 
-    listOfPositions = list(listOfPositions)
-    return len(listOfPositions)
+def getNextPositionsByGrid(lines, currentPositions):
+    groupedPositionsByGrid = more_itertools.bucket(getNextPositionsMult(lines, currentPositions), key = lambda x: calculateGridPosition(x, lines))
+    r = dict()
+    for k in groupedPositionsByGrid:
+        r.update({k : set(groupedPositionsByGrid[k])})
+    return r
 
-@functools.lru_cache
-def solveFromPositions(currentPositions, lines):
-    currentGridPosition = calculateGridPosition(currentPositions[0], lines)
+def getPositionsByGrid(lines, positions):
+    groupedPositionsByGrid = more_itertools.bucket(positions, key = lambda x: calculateGridPosition(x, lines))
+    r = dict()
+    for k in groupedPositionsByGrid:
+        r.update({k : set(groupedPositionsByGrid[k])})
+    return r
 
-    previousPositionsInGridPosition = []
-    currentPositionsInCurrentGridPosition = {*filter(lambda x: calculateGridPosition(x, lines) == currentGridPosition, currentPositions)}
-    numberOfRepeats = 0
+def getSolution(lines, startPositions, numberOfSteps, visitedGrids = set()):
+    assert len(startPositions) > 0
+    nextPositions = getNextPositionsMult(lines, startPositions)
+    currentGrid = calculateGridPosition(next(iter(startPositions)), lines)
 
+    previousPositionsInGrid = set()
+    currentPositions = {*startPositions}
+    if numberOfSteps == 0:
+        return len(currentPositions)
+    
+    counter = 0
+    nextPositions = getNextPositionsMult(lines, currentPositions) 
+
+    while (nextPositionsInGrid := getPositionsByGrid(lines, nextPositions)[currentGrid]) != previousPositionsInGrid and counter < numberOfSteps:
+        previousPositionsInGrid = getPositionsByGrid(lines, currentPositions)[currentGrid]
+        currentPositions = nextPositions
+        nextPositions = getNextPositionsMult(lines, currentPositions)
+        counter += 1
+
+    visitedGrids.add(currentGrid)
+
+    numberOfPositionsInCurrentGrid = len(getPositionsByGrid(lines, currentPositions)[currentGrid])
+    if counter == numberOfSteps:
+        s = numberOfPositionsInCurrentGrid
+    else:
+        s = len(previousPositionsInGrid) if numberOfSteps % 2 == 0 else numberOfPositionsInCurrentGrid
+
+    for gridId, numberOfStepsToGrid, newStartPositions in map(lambda x: (x[0], x[1][0], x[1][1]), filter(lambda x: x[1][0] < counter, getStepsToAdjecentGrids(lines, startPositions).items())):
+        if gridId not in visitedGrids:
+            s += getSolution(lines, newStartPositions, numberOfSteps - numberOfStepsToGrid, visitedGrids = visitedGrids)
+
+    return s
+    
+def getStepsToAdjecentGrids(lines, startPositions):
+    assert len(startPositions) > 0
+    currentGrid = calculateGridPosition(next(iter(startPositions)), lines)
     adjecentGrids = {
-        (currentGridPosition[0] - 1, currentGridPosition[1] - 1) : None,
-        (currentGridPosition[0] - 1, currentGridPosition[1]) : None,
-        (currentGridPosition[0] - 1, currentGridPosition[1] + 1) : None,
-        (currentGridPosition[0], currentGridPosition[1] + 1) : None,
-        (currentGridPosition[0] + 1, currentGridPosition[1] + 1) : None,
-        (currentGridPosition[0] + 1, currentGridPosition[1]) : None,
-        (currentGridPosition[0] + 1, currentGridPosition[1] - 1) : None,
-        (currentGridPosition[0], currentGridPosition[1] - 1) : None,
+        (currentGrid[0] - 1, currentGrid[1] - 1): None,
+        (currentGrid[0] - 1, currentGrid[1]): None,
+        (currentGrid[0] - 1, currentGrid[1] + 1): None,
+        (currentGrid[0], currentGrid[1] + 1): None,
+        (currentGrid[0] + 1, currentGrid[1] + 1): None,
+        (currentGrid[0] + 1, currentGrid[1]): None,
+        (currentGrid[0] + 1, currentGrid[1] - 1): None,
+        (currentGrid[0], currentGrid[1] - 1): None,
     }
 
-    firstPgpi = None
-    firstNumberOfRepeats = None
+    numberOfSteps = 0
+    currentPositions = {*startPositions}
+    while any(v == None for v in adjecentGrids.values()):
+        currentPositions = getNextPositionsMult(lines, currentPositions)
+        numberOfSteps += 1
+        for k, v in filter(lambda x: x[0] in adjecentGrids and adjecentGrids[x[0]] == None, getPositionsByGrid(lines, currentPositions).items()):
+            assert len(v) > 0
+            adjecentGrids[k] = (numberOfSteps, list(v))
 
-    while (pgpi := next((i for i, pgp in enumerate(previousPositionsInGridPosition) if currentPositionsInCurrentGridPosition == pgp), None)) == None or not all(v != None for v in adjecentGrids.values()):
-        previousPositionsInGridPosition.append(currentPositionsInCurrentGridPosition)
-        if pgpi != None and firstPgpi == None:
-            firstPgpi = pgpi
-            firstNumberOfRepeats = numberOfRepeats
-
-        restList = list(filter(lambda x: calculateGridPosition(x, lines) != currentGridPosition, currentPositions))
-        restList.sort(key = lambda x: calculateGridPosition(x, lines))
-        rest = {k : list(v) for k, v in itertools.groupby(restList, key = lambda x: calculateGridPosition(x, lines))}
-        for k, v in rest.items():
-            if k in adjecentGrids and adjecentGrids[k] == None:
-                adjecentGrids[k] = (numberOfRepeats, v)
-
-        currentPositions = list({*itertools.chain.from_iterable(getNextPositions(position, lines) for position in currentPositions)})
-        currentPositionsInCurrentGridPosition = {*filter(lambda x: calculateGridPosition(x, lines) == currentGridPosition, currentPositions)}
-        numberOfRepeats += 1
-
-    if pgpi != None and firstPgpi == None:
-        firstPgpi = pgpi
-        firstNumberOfRepeats = numberOfRepeats
-
-    return (firstNumberOfRepeats, tuple(map(lambda x: len(x), previousPositionsInGridPosition[pgpi:firstNumberOfRepeats])), adjecentGrids)
+    return adjecentGrids
 
 
 def main():
-    lines = tuple(map(lambda x: tuple(x.strip()), file.readlines()))
-    assert all(len(lines[0]) == len(line) for line in lines)
+    lines = list(map(lambda x: x.strip(), file.readlines()))
+    testData = [
+        # (6, 16),
+        # (10, 50),
+        (50, 1594),
+        (100, 6536),
+        (5000, 16733044)
+    ]
 
-    sLocation = findS(lines)
-    visited = set()
-    gridLocationInit = 0
-    totalCount = 0
-    maxSteps = 50
-
-    previousRepeats = 0
-    previousOuterGrid = []
-
-    outerGrid = [(0, [sLocation], gridLocationInit)]
-    while totalCount < maxSteps:
-        previousTotalCount = totalCount
-        previousOuterGrid = outerGrid 
-
-        newOuterGrid = []
-        for (locations, gridLocation) in outerGrid:
-            currentGridPosition = calculateGridPosition(locations[0], lines)
-            visited.add(currentGridPosition)
-
-            (numberOfRepeats, repeats, rest) = solveFromPositions(tuple(locations), lines)
-
-            totalCount += numberOfRepeats
-
-            for (gridLocation, (numberOfRepeatsTillGridLocation, locations)) in rest.items():
-                if gridLocation in visited:
-                    continue
-                
-                if gridLocation in map(lambda x: x[1], newOuterGrid):
-                    continue
-
-                newOuterGrid.append((numberOfRepeatsTillGridLocation, locations, gridLocation))
-        outerGrid = newOuterGrid
-
-    finalTotal = previousOuterGrid
-    for (locations, gridId) for previousOuterGrid:
-        assert len(locations) == 0
-        finalTotal += 
-
-    print(f'{previousTotalCount}, {totalCount}')
-    print(f'{list(map(lambda x: x[1], previousOuterGrid))}, {list(map(lambda x: x[1], outerGrid))}')
+    startPosition = findS(lines)
+    for numberOfSteps, expected in testData:
+        solution = getSolution(lines, {startPosition}, numberOfSteps, visitedGrids = set())
+        print(f'{solution} -> {expected}')
 
 if __name__ == '__main__':
     main()
